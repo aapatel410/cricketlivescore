54569e38b039dd191ab66f63bc53845a67962ccf
Aakashkumar <aapatel410@gmail.com>
2020-10-13 20:29:48 -0400

    Updates

---
+++ precache-manifest.5f331561c012e3090395ba29267a2b3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91f2498509cf126c1160ee76aad25cd0",
+    "url": "/cricketlivescore%20/index.html"
+  },
+  {
+    "revision": "3476279391c1172ede81",
+    "url": "/cricketlivescore%20/static/css/main.37dad6c0.chunk.css"
+  },
+  {
+    "revision": "e28d8a2c110a10c7d46b",
+    "url": "/cricketlivescore%20/static/js/2.6b86b4a9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/cricketlivescore%20/static/js/2.6b86b4a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3476279391c1172ede81",
+    "url": "/cricketlivescore%20/static/js/main.793884fb.chunk.js"
+  },
+  {
+    "revision": "73502080dd50a335e64c",
+    "url": "/cricketlivescore%20/static/js/runtime-main.abefb8d6.js"
+  },
+  {
+    "revision": "ce42695bf08c2b22ec298b5ecd756545",
+    "url": "/cricketlivescore%20/static/media/vs.ce42695b.jpg"
+  }
+]);
